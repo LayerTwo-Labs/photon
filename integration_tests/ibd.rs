LayerTwo-Labs/thunder-rust

Update to latest bip300301_enforcer

integration_tests/ibd.rs
@@ -35,7 +35,7 @@ async fn setup(
     res_tx: mpsc::UnboundedSender<anyhow::Result<()>>,
 ) -> anyhow::Result<(EnforcerPostSetup, ThunderNodes)> {
     let enforcer_pre_setup =
-        EnforcerPreSetup::new(bin_paths.others, Network::Regtest)?;
+        EnforcerPreSetup::new(&bin_paths.others, Network::Regtest)?;
     let mut enforcer_post_setup = {
         let setup_opts: EnforcerSetupOpts = Default::default();
         enforcer_pre_setup
@@ -44,7 +44,7 @@ async fn setup(
     };
     let sidechain_sender = PostSetup::setup(
         Init {
-            thunder_app: bin_paths.thunder.clone(),
+            thunder_app: bin_paths.thunder()?.clone(),
             data_dir_suffix: Some("sender".to_owned()),
         },
         &enforcer_post_setup,
@@ -54,7 +54,7 @@ async fn setup(
     tracing::info!("Setup thunder send node successfully");
     let sidechain_syncer = PostSetup::setup(
         Init {
-            thunder_app: bin_paths.thunder.clone(),
+            thunder_app: bin_paths.thunder()?.clone(),
             data_dir_suffix: Some("syncer".to_owned()),
         },
         &enforcer_post_setup,

integration_tests/integration_test.rs
@@ -133,7 +133,7 @@ fn deposit_withdraw_roundtrip_trial(
         async move {
             let (res_tx, _) = futures::channel::mpsc::unbounded();
             let pre_setup =
-                EnforcerPreSetup::new(bin_paths.others, Network::Regtest)?;
+                EnforcerPreSetup::new(&bin_paths.others, Network::Regtest)?;
             let post_setup = {
                 let setup_opts: EnforcerSetupOpts = Default::default();
                 pre_setup
@@ -143,7 +143,7 @@ fn deposit_withdraw_roundtrip_trial(
             deposit_withdraw_roundtrip(
                 post_setup,
                 Init {
-                    thunder_app: bin_paths.thunder,
+                    thunder_app: bin_paths.thunder()?.clone(),
                     data_dir_suffix: None,
                 },
                 res_tx,

integration_tests/unknown_withdrawal.rs
@@ -30,7 +30,7 @@ use crate::{
 
 /// Initial setup for the test
 async fn setup(
-    enforcer_bin_paths: EnforcerBinPaths,
+    enforcer_bin_paths: &EnforcerBinPaths,
     res_tx: mpsc::UnboundedSender<anyhow::Result<()>>,
 ) -> anyhow::Result<EnforcerPostSetup> {
     let enforcer_pre_setup =
@@ -59,10 +59,10 @@ async fn unknown_withdrawal_task(
     res_tx: mpsc::UnboundedSender<anyhow::Result<()>>,
 ) -> anyhow::Result<()> {
     let mut enforcer_post_setup =
-        setup(bin_paths.others, res_tx.clone()).await?;
+        setup(&bin_paths.others, res_tx.clone()).await?;
     let mut sidechain_withdrawer = PostSetup::setup(
         Init {
-            thunder_app: bin_paths.thunder.clone(),
+            thunder_app: bin_paths.thunder()?.clone(),
             data_dir_suffix: Some("withdrawer".to_owned()),
         },
         &enforcer_post_setup,
@@ -93,7 +93,7 @@ async fn unknown_withdrawal_task(
     // New sidechain node, starting from scratch
     let mut sidechain_successor = PostSetup::setup(
         Init {
-            thunder_app: bin_paths.thunder,
+            thunder_app: bin_paths.thunder()?.clone(),
             data_dir_suffix: Some("successor".to_owned()),
         },
         &enforcer_post_setup,

integration_tests/util.rs
@@ -1,34 +1,24 @@
 use std::{
     ffi::{OsStr, OsString},
     path::PathBuf,
+    sync::OnceLock,
 };
 
 use bip300301_enforcer_integration_tests::util::{
-    AbortOnDrop, BinPaths as EnforcerBinPaths, VarError, get_env_var,
+    AbortOnDrop, BinPaths as EnforcerBinPaths, OnceLockExt as _, VarError,
     spawn_command_with_args,
 };
 use thunder::types::Network;
 
-fn load_env_var_from_string(s: &str) -> Result<(), VarError> {
-    dotenvy::from_read_override(s.as_bytes())
-        .map_err(|err| VarError::new(s, err))
-}
-
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 pub struct BinPaths {
-    pub thunder: PathBuf,
+    thunder: OnceLock<PathBuf>,
     pub others: EnforcerBinPaths,
 }
 
 impl BinPaths {
-    /// Read from environment variables
-    pub fn from_env() -> Result<Self, VarError> {
-        let () = load_env_var_from_string("BITCOIN_UTIL=''")?;
-        let () = load_env_var_from_string("SIGNET_MINER=''")?;
-        Ok(Self {
-            thunder: get_env_var("THUNDER_APP")?.into(),
-            others: EnforcerBinPaths::from_env()?,
-        })
+    pub fn thunder(&self) -> Result<&PathBuf, VarError> {
+        self.thunder.get_or_try_init_from_env("THUNDER_APP")
     }
 }
 

LayerTwo-Labs/photon

integration_tests/ibd.rs
@@ -35,7 +35,7 @@ async fn setup(
     res_tx: mpsc::UnboundedSender<anyhow::Result<()>>,
 ) -> anyhow::Result<(EnforcerPostSetup, PhotonNodes)> {
     let enforcer_pre_setup =
-        EnforcerPreSetup::new(bin_paths.others, Network::Regtest)?;
+        EnforcerPreSetup::new(&bin_paths.others, Network::Regtest)?;
     let mut enforcer_post_setup = {
         let setup_opts: EnforcerSetupOpts = Default::default();
         enforcer_pre_setup
@@ -44,7 +44,7 @@ async fn setup(
     };
     let sidechain_sender = PostSetup::setup(
         Init {
-            photon_app: bin_paths.photon.clone(),
+            photon_app: bin_paths.photon()?.clone(),
             data_dir_suffix: Some("sender".to_owned()),
         },
         &enforcer_post_setup,
@@ -54,7 +54,7 @@ async fn setup(
     tracing::info!("Setup photon send node successfully");
     let sidechain_syncer = PostSetup::setup(
         Init {
-            photon_app: bin_paths.photon.clone(),
+            photon_app: bin_paths.photon()?.clone(),
             data_dir_suffix: Some("syncer".to_owned()),
         },
         &enforcer_post_setup,

integration_tests/integration_test.rs
@@ -133,7 +133,7 @@ fn deposit_withdraw_roundtrip_trial(
         async move {
             let (res_tx, _) = futures::channel::mpsc::unbounded();
             let pre_setup =
-                EnforcerPreSetup::new(bin_paths.others, Network::Regtest)?;
+                EnforcerPreSetup::new(&bin_paths.others, Network::Regtest)?;
             let post_setup = {
                 let setup_opts: EnforcerSetupOpts = Default::default();
                 pre_setup
@@ -143,7 +143,7 @@ fn deposit_withdraw_roundtrip_trial(
             deposit_withdraw_roundtrip(
                 post_setup,
                 Init {
-                    photon_app: bin_paths.photon,
+                    photon_app: bin_paths.photon()?.clone(),
                     data_dir_suffix: None,
                 },
                 res_tx,

integration_tests/unknown_withdrawal.rs
@@ -30,7 +30,7 @@ use crate::{
 
 /// Initial setup for the test
 async fn setup(
-    enforcer_bin_paths: EnforcerBinPaths,
+    enforcer_bin_paths: &EnforcerBinPaths,
     res_tx: mpsc::UnboundedSender<anyhow::Result<()>>,
 ) -> anyhow::Result<EnforcerPostSetup> {
     let enforcer_pre_setup =
@@ -59,10 +59,10 @@ async fn unknown_withdrawal_task(
     res_tx: mpsc::UnboundedSender<anyhow::Result<()>>,
 ) -> anyhow::Result<()> {
     let mut enforcer_post_setup =
-        setup(bin_paths.others, res_tx.clone()).await?;
+        setup(&bin_paths.others, res_tx.clone()).await?;
     let mut sidechain_withdrawer = PostSetup::setup(
         Init {
-            photon_app: bin_paths.photon.clone(),
+            photon_app: bin_paths.photon()?.clone(),
             data_dir_suffix: Some("withdrawer".to_owned()),
         },
         &enforcer_post_setup,
@@ -93,7 +93,7 @@ async fn unknown_withdrawal_task(
     // New sidechain node, starting from scratch
     let mut sidechain_successor = PostSetup::setup(
         Init {
-            photon_app: bin_paths.photon,
+            photon_app: bin_paths.photon()?.clone(),
             data_dir_suffix: Some("successor".to_owned()),
         },
         &enforcer_post_setup,

integration_tests/util.rs
@@ -1,34 +1,24 @@
 use std::{
     ffi::{OsStr, OsString},
     path::PathBuf,
+    sync::OnceLock,
 };
 
 use bip300301_enforcer_integration_tests::util::{
-    AbortOnDrop, BinPaths as EnforcerBinPaths, VarError, get_env_var,
+    AbortOnDrop, BinPaths as EnforcerBinPaths, OnceLockExt as _, VarError,
     spawn_command_with_args,
 };
 use photon::types::Network;
 
-fn load_env_var_from_string(s: &str) -> Result<(), VarError> {
-    dotenvy::from_read_override(s.as_bytes())
-        .map_err(|err| VarError::new(s, err))
-}
-
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 pub struct BinPaths {
-    pub photon: PathBuf,
+    photon: OnceLock<PathBuf>,
     pub others: EnforcerBinPaths,
 }
 
 impl BinPaths {
-    /// Read from environment variables
-    pub fn from_env() -> Result<Self, VarError> {
-        let () = load_env_var_from_string("BITCOIN_UTIL=''")?;
-        let () = load_env_var_from_string("SIGNET_MINER=''")?;
-        Ok(Self {
-            photon: get_env_var("PHOTON_APP")?.into(),
-            others: EnforcerBinPaths::from_env()?,
-        })
+    pub fn photon(&self) -> Result<&PathBuf, VarError> {
+        self.photon.get_or_try_init_from_env("PHOTON_APP")
     }
 }
 