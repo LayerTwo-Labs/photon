LayerTwo-Labs/thunder-rust

JSONRPC: add get_transaction method

app/rpc_server.rs
@@ -11,7 +11,7 @@ use thunder::{
     types::{Address, PointedOutput, Txid, WithdrawalBundle},
     wallet::Balance,
 };
-use thunder_app_rpc_api::RpcServer;
+use thunder_app_rpc_api::{GetTransactionResponse, RpcServer};
 use tower_http::{
     cors::CorsLayer,
     request_id::{
@@ -141,6 +141,19 @@ impl RpcServer for RpcServerImpl {
 
     async fn get_new_address(&self) -> RpcResult<Address> {
         self.app.wallet.get_new_address().map_err(custom_err)
+    }
+
+    async fn get_transaction(
+        &self,
+        txid: Txid,
+    ) -> RpcResult<Option<GetTransactionResponse>> {
+        let res = self
+            .app
+            .node
+            .try_get_transaction(txid)
+            .map_err(custom_err)?
+            .map(|(tx, block_hash)| GetTransactionResponse { tx, block_hash });
+        Ok(res)
     }
 
     async fn get_wallet_addresses(&self) -> RpcResult<Vec<Address>> {

rpc-api/lib.rs
@@ -4,16 +4,26 @@ use std::net::SocketAddr;
 
 use jsonrpsee::{core::RpcResult, proc_macros::rpc};
 use l2l_openapi::open_api;
+use serde::{Deserialize, Serialize};
 use thunder::{
     net::Peer,
     types::{
-        Address, MerkleRoot, OutPoint, Output, OutputContent, PointedOutput,
-        Txid, WithdrawalBundle, schema as thunder_schema,
+        Address, BlockHash, MerkleRoot, OutPoint, Output, OutputContent,
+        PointedOutput, Transaction, Txid, WithdrawalBundle,
+        schema as thunder_schema,
     },
     wallet::Balance,
 };
+use utoipa::ToSchema;
 
 mod schema;
+
+#[derive(Clone, Debug, Deserialize, Serialize, ToSchema)]
+pub struct GetTransactionResponse {
+    pub tx: Transaction,
+    /// Block hash, if in the active chain
+    pub block_hash: Option<BlockHash>,
+}
 
 #[open_api(ref_schemas[
     Address, MerkleRoot, OutPoint, Output, OutputContent, Txid,
@@ -108,6 +118,13 @@ pub trait Rpc {
     /// Get a new address
     #[method(name = "get_new_address")]
     async fn get_new_address(&self) -> RpcResult<Address>;
+
+    /// Get transaction by txid
+    #[method(name = "get_transaction")]
+    async fn get_transaction(
+        &self,
+        txid: Txid,
+    ) -> RpcResult<Option<GetTransactionResponse>>;
 
     /// Get wallet addresses, sorted by base58 encoding
     #[method(name = "get_wallet_addresses")]

LayerTwo-Labs/photon

app/rpc_server.rs
@@ -11,7 +11,7 @@ use photon::{
     types::{Address, PointedOutput, Txid, WithdrawalBundle},
     wallet::Balance,
 };
-use photon_app_rpc_api::RpcServer;
+use photon_app_rpc_api::{GetTransactionResponse, RpcServer};
 use tower_http::{
     cors::CorsLayer,
     request_id::{
@@ -141,6 +141,19 @@ impl RpcServer for RpcServerImpl {
 
     async fn get_new_address(&self) -> RpcResult<Address> {
         self.app.wallet.get_new_address().map_err(custom_err)
+    }
+
+    async fn get_transaction(
+        &self,
+        txid: Txid,
+    ) -> RpcResult<Option<GetTransactionResponse>> {
+        let res = self
+            .app
+            .node
+            .try_get_transaction(txid)
+            .map_err(custom_err)?
+            .map(|(tx, block_hash)| GetTransactionResponse { tx, block_hash });
+        Ok(res)
     }
 
     async fn get_wallet_addresses(&self) -> RpcResult<Vec<Address>> {

rpc-api/lib.rs
@@ -7,13 +7,23 @@ use l2l_openapi::open_api;
 use photon::{
     net::Peer,
     types::{
-        Address, MerkleRoot, OutPoint, Output, OutputContent, PointedOutput,
-        Txid, WithdrawalBundle, schema as photon_schema,
+        Address, BlockHash, MerkleRoot, OutPoint, Output, OutputContent,
+        PointedOutput, Transaction, Txid, WithdrawalBundle,
+        schema as photon_schema,
     },
     wallet::Balance,
 };
+use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
 
 mod schema;
+
+#[derive(Clone, Debug, Deserialize, Serialize, ToSchema)]
+pub struct GetTransactionResponse {
+    pub tx: Transaction,
+    /// Block hash, if in the active chain
+    pub block_hash: Option<BlockHash>,
+}
 
 #[open_api(ref_schemas[
     Address, MerkleRoot, OutPoint, Output, OutputContent, Txid,
@@ -108,6 +118,13 @@ pub trait Rpc {
     /// Get a new address
     #[method(name = "get_new_address")]
     async fn get_new_address(&self) -> RpcResult<Address>;
+
+    /// Get transaction by txid
+    #[method(name = "get_transaction")]
+    async fn get_transaction(
+        &self,
+        txid: Txid,
+    ) -> RpcResult<Option<GetTransactionResponse>>;
 
     /// Get wallet addresses, sorted by base58 encoding
     #[method(name = "get_wallet_addresses")]